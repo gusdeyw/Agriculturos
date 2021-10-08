8956ad72c6f021e5720a8607065b0605a6440cad
Gusde Widnyana <widnyana.bagus@gmail.com>
2021-10-08 13:19:35 +0800

    Testing Tailwind CSS

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <title>Agriculturos</title>
+</head>
+<body>
+    
+<div class="grid grid-cols-2">
+    <div class="container">1</div>
+    <div class="container bg-gray-300">2</div>
+
+</div>
+
+</body>
+</html>
